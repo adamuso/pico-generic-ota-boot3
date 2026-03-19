fddcc4c91e646dc4559e4f28ad30a9c7d467c4e8
Adam Ogiba <adamek.og@gmail.com>
2026-03-19 20:28:05 +0100

    Add boot3_slot_state support and clear pending state option

---
+++ src/boot3_slot_state.c
@@ -0,0 +1,10 @@
+/* Copyright (c) 2026 Adam Ogiba - Licensed under MIT */
+
+// TODO: Somehow get this from project config instead of hardcoding it here
+#define PICO_RP2040 1
+
+#include "hardware/flash.h"
+
+#if BOOT3_CLEAR_PENDING_STATE
+const uint8_t boot3_checksum[FLASH_SECTOR_SIZE * 2] __attribute__((section(".boot3_slot_state"))) = { [0 ... FLASH_SECTOR_SIZE * 2 - 1] = 0xFF };
+#endif
